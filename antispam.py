b31ed7a6ace88bd7d40725b939e5589094ac4555
ItzSplash <27790761+ItzSplash@users.noreply.github.com>
2017-10-30 10:09:20 -0400

    Update antispam.py

--- antispam.py
+++ antispam.py
@@ -6,16 +6,20 @@ import random
 import asyncio
 from discord.ext import commands
 
+#you can change the prefix to what you want, just keep it inside the quotations
 bot =commands.Bot(command_prefix ="!")
 
 @bot.event
 async def on_ready():
+    #what the terminal say
     print("Anti-Spam Bot is Online!, running version " + discord.__version__ + " of Discord.py")
     await asyncio.sleep(10)
+    #this sets what tame the bot is playing
     await bot.change_presence(game=discord.Game(name="https://github.com/ItzSplash/antispam", type=1))
 
 @bot.command()
 async def ping():
     await bot.say("pong!")
 
+    #insert your bot token here inside the quotations
 bot.run("bot token")
